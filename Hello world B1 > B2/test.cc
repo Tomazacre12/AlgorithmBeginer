7e2e5cce4ff0c97690fc1ce0a89b39284f1d1d0b
Ha Vu Hieu <hieuhv@dinoland.io>
2022-09-28 21:49:59 +0700

    add lesson Hello world B1 > B2

---
+++ Hello world B1 > B2/test.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    // bat dau chuong trinh
+
+    printf("Hello\n");
+    printf("Cong hoa xa hoi chu nghia viet nam\n");
+    printf("Doc Lap *Tu do* Hanh phuc\n");
+
+	// ket thuc chuong trinh
+    return 0;
+}
