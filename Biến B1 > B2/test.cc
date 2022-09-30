5c17f9133785f7df644247293aa563418ae8363f
Ha Vu Hieu <hieuhv@dinoland.io>
2022-09-30 20:30:12 +0700

    add lesson Biến B1 > B2

---
+++ Biến B1 > B2/test.cc
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    // bat dau chuong trinh
+
+    int myAge = 8;
+    int myAge = myAge + 10;
+    int motherAge = myAge * 3;
+
+	// ket thuc chuong trinh
+    return 0;
+}
