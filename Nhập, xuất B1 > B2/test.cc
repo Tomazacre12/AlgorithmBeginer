3483dd940e469307edeca4da1c427994251c0dfd
Ha Vu Hieu <hieuhv@dinoland.io>
2022-10-03 23:24:40 +0700

    add lesson nhap. xuat B1 > B2

---
+++ Nhập, xuất B1 > B2/test.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<fstream>
+#include<string.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+    // bat dau chuong trinh
+        freopen("test.inp","r",stdin);
+
+        int a,b;
+        scanf('%d %d', &a, &b);
+
+        int tong = a+b;
+        printf('Tong hai so la: %d\n', tong);
+        return 0;
+    
+
+	// ket thuc chuong trinh
+    return 0;
+}
